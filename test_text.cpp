f12743bab591e269a76e342d1f26354bf2e60df0
mendojen <mendojen@onid.oregonstate.edu>
2016-10-16 13:26:39 -0700

    Updated files 10_16

--- test_text.cpp
+++ test_text.cpp
@@ -1,37 +1,75 @@
 // reading a text file
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
 using namespace std;
 //create Rooms class with all different components
 
 class Rooms
 {
-	string roomName;
-	string Ldesc;
+	public:
+		Rooms(string array[9])
+		{
+			cout<<"check2"<<endl;
+			setName(array[0]);
+		}
+		void setName(string name)
+		{
+			cout<<"check3"<<endl;
+			roomName=name;
+		}
+		string getName()
+		{
+			return roomName;
+		}
+
+	
+	private:
+		string roomName;
+		string Ldesc;
 
 };
 //save contexts of txt files into arrays within rooms class
 int main () {
-string line;
-string rooms[15];
-string delimiter =":";
-//http://stackoverflow.com/questions/32766817/go-to-a-specific-line-in-file-and-read-it
-      ifstream file("room1.txt");
+	string line;
+	string roomArr[9];
+	string delimiter =":";
+	vector<Rooms> roomList;
+	//http://stackoverflow.com/questions/32766817/go-to-a-specific-line-in-file-and-read-it
+	for (int a=1; a<3;a++)
+	{	
+		stringstream ss;
+		ss<<a;
+		string str=ss.str();
+		string filename="room"+str+".txt";
 
-        unsigned int line_number(1);
-        const unsigned int requested_line_number(2);
-        while (std::getline(file, line))
-        {
-                if (line_number == requested_line_number)
-                {
-					string s=line;
-					int pos = s.find(":");
-					string token = s.substr (pos+1);
-					rooms[1]=token;
-				}
-                line_number++;
-        }
-		cout<<"room[1] array:"<<rooms[1]<<endl;
-  return 0;
+		ifstream file(filename.c_str());
+		unsigned int line_number(1);
+			int atLine=1;
+			while (std::getline(file, line))
+			{
+					if (line_number == atLine)
+					{
+						string s=line;
+						int pos = s.find(":");
+						string token = s.substr (pos+1);
+						roomArr[atLine-1]=token;
+					}      
+					line_number++;
+					atLine++;
+			}
+			cout<<"file "<<a<<endl;
+			for (int i=0; i<atLine-1;i++)
+			{
+				cout<<"room "<<i<<" array:"<<roomArr[i]<<endl;
+			}
+			cout<<"check1"<<endl;
+		//Insert call to constructor to create objects here. Objects are created as files are read
+		Rooms temp(roomArr);
+		roomList.push_back(temp);
+	}
+	cout<<roomList[1].getName()<<endl;
+	return 0;
 }
