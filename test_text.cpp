8be6b021652b25b2992034ea01f4f840e6c13818
mendojen <mendojen@onid.oregonstate.edu>
2016-10-17 16:55:10 -0700

    include header files

--- test_text.cpp
+++ test_text.cpp
@@ -4,40 +4,20 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include "rooms.h"
 using namespace std;
-//create Rooms class with all different components
 
-class Rooms
-{
-	public:
-		Rooms(string array[9])
-		{
-			cout<<"check2"<<endl;
-			setName(array[0]);
-		}
-		void setName(string name)
-		{
-			cout<<"check3"<<endl;
-			roomName=name;
-		}
-		string getName()
-		{
-			return roomName;
-		}
 
-	
-	private:
-		string roomName;
-		string Ldesc;
 
-};
 //save contexts of txt files into arrays within rooms class
 int main () {
 	string line;
 	string roomArr[9];
 	string delimiter =":";
+	//vector contains all Rooms objects
 	vector<Rooms> roomList;
 	//http://stackoverflow.com/questions/32766817/go-to-a-specific-line-in-file-and-read-it
+	//read files in 1 at a time
 	for (int a=1; a<3;a++)
 	{	
 		stringstream ss;
@@ -50,16 +30,18 @@ int main () {
 			int atLine=1;
 			while (std::getline(file, line))
 			{
-					if (line_number == atLine)
-					{
-						string s=line;
-						int pos = s.find(":");
-						string token = s.substr (pos+1);
-						roomArr[atLine-1]=token;
-					}      
-					line_number++;
-					atLine++;
+				//saves each line in an array
+				if (line_number == atLine)
+				{
+					string s=line;
+					int pos = s.find(":");
+					string token = s.substr (pos+1);
+					roomArr[atLine-1]=token;
+				}      
+				line_number++;
+				atLine++;
 			}
+			//display's array contents for testing
 			cout<<"file "<<a<<endl;
 			for (int i=0; i<atLine-1;i++)
 			{
@@ -70,6 +52,7 @@ int main () {
 		Rooms temp(roomArr);
 		roomList.push_back(temp);
 	}
+	//retrieves object information in vector
 	cout<<roomList[1].getName()<<endl;
 	return 0;
 }
