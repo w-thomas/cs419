c5f15c77e72e3e0f5d27e2d5daa741f90014cc8e
Will Thomas <fastnoid@gmail.com>
2016-10-20 18:39:58 -0700

    more work on text input

--- interface.cpp
+++ interface.cpp
@@ -53,6 +53,8 @@ WINDOW *create_newwin(int height, int width, int starty, int startx, bool border
 std::string getInput() {
 	char * input = new char[80];
 
+	//This is hacky, but it "clears" the input line of last input by writing over
+	//it with empty space.
 	mvprintw(LINES - 2, 0, "Your input:                                                       ");
 
 	mvprintw(LINES - 2, 0, "Your input: ");
@@ -72,17 +74,7 @@ void destroy_win(WINDOW *local_win) {
 	 * and so an ugly remnant of window. 
 	 */
 	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-	/* The parameters taken are 
-	 * 1. win: the window on which to operate
-	 * 2. ls: character to be used for the left side of the window 
-	 * 3. rs: character to be used for the right side of the window 
-	 * 4. ts: character to be used for the top side of the window 
-	 * 5. bs: character to be used for the bottom side of the window 
-	 * 6. tl: character to be used for the top left corner of the window 
-	 * 7. tr: character to be used for the top right corner of the window 
-	 * 8. bl: character to be used for the bottom left corner of the window 
-	 * 9. br: character to be used for the bottom right corner of the window
-	 */
+	
 	//wrefresh(local_win);
 	delwin(local_win);
 }
