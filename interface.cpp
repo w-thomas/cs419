bcc7120622ec2888f908f1107d906ef0cd8fcdfc
Will Thomas <fastnoid@gmail.com>
2016-10-27 19:16:30 -0700

    Added support for color

--- interface.cpp
+++ interface.cpp
@@ -3,6 +3,26 @@
 void start_interface() {
 	initscr();
 	raw();
+
+Testing for color Capabilities
+	if(has_colors() == FALSE)
+	{	endwin();
+		printf("Your terminal does not support color\n");
+		exit(1);
+	}
+	
+	/*Initializing Colors*/
+	start_color();
+	init_pair (0, COLOR_WHITE, COLOR_BLACK);
+	init_pair (1, COLOR_CYAN, COLOR_BLACK);
+  	init_pair (2, COLOR_WHITE, COLOR_BLUE);
+  	init_pair (3, COLOR_WHITE, COLOR_BLACK);
+  	init_pair (4, COLOR_WHITE, COLOR_RED);
+  	init_pair (5, COLOR_BLACK, COLOR_GREEN);
+  	init_pair (6, COLOR_RED, COLOR_WHITE);
+
+  	wbkgd(stdscr, COLOR_PAIR(0));
+
 	refresh();
 	createDialogueWin();
 	//noecho();
@@ -15,7 +35,9 @@ void end_interface() {
 void createDialogueWin(){
 	WINDOW *local_win;
 
-	local_win = create_newwin((LINES/2)+2, 70, 1, 1, true);
+	local_win = create_newwin((LINES/2)+2, (COLS * 80)/100, 1, 1, true);
+
+	wbkgd(local_win, COLOR_PAIR(4));
 
 	wrefresh(local_win);
 }
@@ -24,12 +46,14 @@ void printString(std::string message) {
 
 	WINDOW *local_win;
 
-	local_win = create_newwin(LINES/2, 68, 2, 2, false);
+	local_win = create_newwin(LINES/2, (COLS*79)/100, 2, 2, false);
+
+	wbkgd(local_win, COLOR_PAIR(6));
 
 	char * cstr = new char[message.length()+1];
 	strcpy (cstr, message.c_str());
 	wprintw(local_win, cstr);
-	//getch();
+
 	wrefresh(local_win);
 
 	destroy_win(local_win);
@@ -82,6 +106,7 @@ void destroy_win(WINDOW *local_win) {
 void printDescLong(){
 	//TODO:
 	//Print room desc object
+
 }
 
 void printHelp() {
@@ -89,17 +114,18 @@ void printHelp() {
 	//convert and print help text
 	WINDOW *local_win;
 
-	local_win = create_newwin(LINES/2, 68, 2, 2, false);
+	local_win = create_newwin(LINES/2, (COLS*79)/100, 2, 2, false);
+
+
 	mvwprintw(local_win, 2, 30, "Help");
 	mvwprintw(local_win, 4, 4, "Look: blah blah blah");
 	mvwprintw(local_win, 6, 4, "Look at <featur || object>: blah blah blah");
+	mvwprintw(local_win, 8, 4, "go <direction> : Lorem Ipsum blah");
+	mvwprintw(local_win, 10, 4, "quit or q : Quit and exit the game");
+	mvwprintw(local_win, 12, 4, "help or h : View this help dialogue again.");
 	wrefresh(local_win);
-	   //  cout << "\n" << "Help\n\n";
-    // cout << "look : blah, blah, blah\n";
-    // cout << "look at <feature || object> : blah, blah, blah\n";
-    // cout << "go <direction> : blah, blah, blah\n";
-    // cout << "quit or q : blah, blah, blah\n";
-    // cout << "help or h : blah, blah, blah\n\n";
+
+
 	destroy_win(local_win);
 }
 
--- interface.hpp
+++ interface.hpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string>
 #include <string.h>
+#include <cstdlib>
 
 
 /**
