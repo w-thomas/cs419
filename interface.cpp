158e8abb53553a070fc2fbb5eb8f314622e654c7
Will Thomas <fastnoid@gmail.com>
2016-10-30 12:29:37 -0700

    Merged in new interface files to integrate with main code

--- interface.cpp
+++ interface.cpp
@@ -3,6 +3,28 @@
 void start_interface() {
 	initscr();
 	raw();
+
+//Testing for color Capabilities
+	if(has_colors() == FALSE)
+	{	endwin();
+		printf("Your terminal does not support color\n");
+		exit(1);
+	}
+	
+	/*Initializing Colors*/
+	start_color();
+	init_pair (0, COLOR_WHITE, COLOR_BLACK);
+	init_pair (1, COLOR_CYAN, COLOR_BLACK);
+  	init_pair (2, COLOR_WHITE, COLOR_BLUE);
+  	init_pair (3, COLOR_WHITE, COLOR_BLACK);
+  	init_pair (4, COLOR_WHITE, COLOR_RED);
+  	init_pair (5, COLOR_BLACK, COLOR_GREEN);
+  	init_pair (6, COLOR_RED, COLOR_WHITE);
+
+  	wbkgd(stdscr, COLOR_PAIR(0));
+
+	refresh();
+	createDialogueWin();
 	//noecho();
 }
 
@@ -10,29 +32,54 @@ void end_interface() {
 	endwin();
 }
 
+void createDialogueWin(){
+	WINDOW *local_win;
+
+	local_win = create_newwin((LINES/2)+2, (COLS * 80)/100, 1, 1, true);
+
+	wbkgd(local_win, COLOR_PAIR(4));
+
+	wrefresh(local_win);
+}
+
 void printString(std::string message) {
+
+	WINDOW *local_win;
+
+	local_win = create_newwin(LINES/2, (COLS*78)/100, 2, 2, false);
+
+	wbkgd(local_win, COLOR_PAIR(6));
+
 	char * cstr = new char[message.length()+1];
 	strcpy (cstr, message.c_str());
-	printw(cstr);
-	//getch();
+	wprintw(local_win, cstr);
+
+	wrefresh(local_win);
+
+	destroy_win(local_win);
 
 	delete(cstr);
 }
 
-WINDOW *create_newwin(int height, int width, int starty, int startx)
-{	WINDOW *local_win;
+WINDOW *create_newwin(int height, int width, int starty, int startx, bool border) {	
+	WINDOW *local_win;
 
 	local_win = newwin(height, width, starty, startx);
-	box(local_win, 0 , 0);		/* 0, 0 gives default characters 
-					 			* for the vertical and horizontal
-					 			* lines			*/
-	wrefresh(local_win);		/* Show that box 		*/
+
+	if(border){
+	//box(local_win, 0 , 0);
+	wborder(local_win, '|', '|', '-', '-', '+', '+', '+', '+');	
+	}
 
 	return local_win;
 }
 
-std::string getInput(){
+std::string getInput() {
 	char * input = new char[80];
+
+	//This is hacky, but it "clears" the input line of last input by writing over
+	//it with empty space.
+	mvprintw(LINES - 2, 0, "Your input:                                                       ");
 
 	mvprintw(LINES - 2, 0, "Your input: ");
 
@@ -43,7 +90,44 @@ std::string getInput(){
 	delete(input);
 
 	return inString;
+}
 
+void destroy_win(WINDOW *local_win) {	
+	/* box(local_win, ' ', ' '); : This won't produce the desired
+	 * result of erasing the window. It will leave it's four corners 
+	 * and so an ugly remnant of window. 
+	 */
+	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+	
+	//wrefresh(local_win);
+	delwin(local_win);
+}
+
+void printDescLong(){
+	//TODO:
+	//Print room desc object
+
+}
+
+void printHelp() {
+	//TODO:
+	//convert and print help text
+	WINDOW *local_win;
+
+	local_win = create_newwin(LINES/2, (COLS*77)/100, 2, 2, false);
+
+	wbkgd(local_win, COLOR_PAIR(5));
+
+	mvwprintw(local_win, 2, 30, "Help");
+	mvwprintw(local_win, 4, 4, "Look: blah blah blah");
+	mvwprintw(local_win, 6, 4, "Look at <featur || object>: blah blah blah");
+	mvwprintw(local_win, 8, 4, "go <direction> : Lorem Ipsum blah");
+	mvwprintw(local_win, 10, 4, "quit or q : Quit and exit the game");
+	mvwprintw(local_win, 12, 4, "help or h : View this help dialogue again.");
+	wrefresh(local_win);
+
+
+	destroy_win(local_win);
 }
 
 
--- interface.hpp
+++ interface.hpp
@@ -5,15 +5,49 @@
 #include <stdio.h>
 #include <string>
 #include <string.h>
+#include <cstdlib>
 
 
+/**
+* initializes ncurses and hides the terminal
+**/
 void start_interface();
+
+/**
+* Ends the curses mode. Must be called at end of program to avoid errors
+**/
 void end_interface();
 
+//TODO: Documentation
+void createDialogueWin();
+
+/**
+* printString(string) - accepts a std::string variable and prints it to the screen
+* handles all conversion to c-string and refreshes the ncurses display.
+* PARAM: std::string
+* RETURN: nothing
+**/
 void printString(std::string message);
 
-WINDOW *create_newwin(int height, int width, int starty, int startx);
+/**
+* Creates an ncurses window at specified location and size
+* PARAM: int values represent number of lines and columns in terminal
+* RETURN: pointer to new ncurses window
+**/
+WINDOW *create_newwin(int height, int width, int starty, int startx, bool border);
 
+//TODO: Documentation
+void destroy_win(WINDOW *local_win);
+
+/**
+* Uses ncurses API to accept input from user. Will accept all text until 
+* user presses enter / return. Input location is at bottom left corner of display
+* PARAM: nothing
+* RETURN: std::string value
+**/
 std::string getInput();
+
+//TODO: Documentation
+void printHelp();
 
 #endif
